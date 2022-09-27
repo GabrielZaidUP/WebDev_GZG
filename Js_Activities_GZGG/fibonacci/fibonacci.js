335cdcce625a4a469fed00579f2e20799d42816e
GabrielZaidUP <112785155+GabrielZaidUP@users.noreply.github.com>
2022-09-27 18:36:08 -0500

    js_activities.1

--- Js_Activities_GZGG/fibonacci/fibonacci.js
+++ Js_Activities_GZGG/fibonacci/fibonacci.js
@@ -22,8 +22,27 @@ function f(n) {
         //TODO: Implement the fibonacci function here!
 
         memo[n] = value;
+        let n1 = 0, n2 = 1, nextN;
+
+console.log('Fibonacci Series:');
+console.log(n1); // print 0
+console.log(n2); // print 1
+
+nextN = n1 + n2;
+
+while (nextN<= value) {
+
+    // print the next term
+    console.log(nextN);
+
+    n1 = n2;
+    n2 = nextN;
+    nextN = n1 + n2;
+}
     }
 
     return value;
 }
-//console.log(fibonacci(15));
+console.log(fibonacci(15));
+
+
